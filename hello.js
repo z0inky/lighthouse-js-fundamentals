b3f585f51d84d204c964a3effc9855192348a079
Micah Lizotte <micah.lizotte@proton.me>
2022-11-10 06:19:47 +0000

    add to hello.js

--- hello.js
+++ hello.js
@@ -6,4 +6,15 @@ sayHello('Caliban');
 sayHello('Miranda');
 sayHello('Ferdinand');
 
+// func prod side effect
+const sayHelloToConsole = function (name) {
+  console.log('Hello, ' + name);
+}
+sayHelloToConsole('John');
 
+// func prod result
+const returnSayHello = function (name) {
+  return 'Hello, ' + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
