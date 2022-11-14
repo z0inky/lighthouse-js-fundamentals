9ceb5fe70c82473e0878175400ae7f11b1e161cc
Micah Lizotte <micah.lizotte@proton.me>
2022-11-14 01:18:08 +0000

    create ice-cream.js

---
+++ ice-cream.js
@@ -0,0 +1,5 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
